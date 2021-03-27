6cc60246fd639a923398713049e5daaa42549638
Nihad Keric <nihadkeric@gmail.com>
2021-03-27 10:33:58 +0100

    added notification on comments data fetching

--- components/input/comments.js
+++ components/input/comments.js
@@ -1,32 +1,46 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useContext } from 'react';
 
 import CommentList from './comment-list';
 import NewComment from './new-comment';
 import classes from './comments.module.css';
+import NotificationContext from '../../store/notification-context';
 
 function Comments(props) {
 	const { eventId } = props;
 
 	const [ showComments, setShowComments ] = useState(false);
 	const [ comments, setComments ] = useState([]);
+	const [ showFetching, setShowFetching ] = useState(false);
+	const notificationCtx = useContext(NotificationContext);
 
 	useEffect(
 		() => {
 			if (showComments) {
-				fetch(`/api/comments/${eventId}`).then((response) => response.json()).then((data) => {
-					setComments(data.comments);
-				});
+				fetchComments();
 			}
 		},
 		[ showComments ]
 	);
+
+	function fetchComments() {
+		setShowFetching(true);
+		fetch(`/api/comments/${eventId}`).then((response) => response.json()).then((data) => {
+			setComments(data.comments);
+			setShowFetching(false);
+		});
+	}
 
 	function toggleCommentsHandler() {
 		setShowComments((prevStatus) => !prevStatus);
 	}
 
 	function addCommentHandler(commentData) {
-		console.log('addCommentHandler');
+		notificationCtx.showNotification({
+			title: 'Signing Up...',
+			message: 'Registerin for newsletter',
+			status: 'pending'
+		});
+
 		fetch(`/api/comments/${eventId}`, {
 			method: 'POST',
 			body: JSON.stringify(commentData),
@@ -34,15 +48,38 @@ function Comments(props) {
 				'Content-Type': 'application/json'
 			}
 		})
-			.then((response) => response.json())
-			.then((data) => console.log(data));
+			.then((response) => {
+				if (response.ok) {
+					return response.json();
+				}
+
+				return response.json().then((data) => {
+					throw new Error(data.message || 'Something went wrong!');
+				});
+			})
+			.then((data) => {
+				fetchComments();
+				notificationCtx.showNotification({
+					title: 'Success',
+					message: 'Successfully added new Comment!',
+					status: 'success'
+				});
+			})
+			.catch((error) => {
+				notificationCtx.showNotification({
+					title: 'Error',
+					message: error.message || 'Something went wrong!',
+					status: 'error'
+				});
+			});
 	}
 
 	return (
 		<section className={classes.comments}>
 			<button onClick={toggleCommentsHandler}>{showComments ? 'Hide' : 'Show'} Comments</button>
 			{showComments && <NewComment onAddComment={addCommentHandler} />}
-			{showComments && <CommentList items={comments} />}
+			{showComments && !showFetching && <CommentList items={comments} />}
+			{showComments && showFetching && <p>Loading ...</p>}
 		</section>
 	);
 }
